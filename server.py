6e63f20ee9fe1d85045930d650e31f6071e9d138
Phatcraft <phathong0727@gmail.com>
2025-11-22 18:15:09 +0700

    main server

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
 import os
 from flask import Flask, render_template
 from dotenv import load_dotenv
+import mysql.connector
+
+# Load enviroment
+load_dotenv()
 
 # Create Flask server
 server = Flask(__name__)
 
-# Load enviroment
-load_dotenv()
+# Connect to MySQL
+conn = mysql.connector.connect(
+  host=os.getenv("SQL_HOST"),
+  user=os.getenv("SQL_USER"),
+  password=os.getenv("SQL_PASS"),
+  database=os.getenv("SQL_DATABASE")
+)
+cursor = conn.cursor()
+
 
 # Home route
 @server.route("/")
